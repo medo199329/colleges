6197bbc046318a36f786b658f88036d1600e9d0b
MohammedSalah8 <>
2021-05-05 13:15:48 +0000

    new js

---
+++ app.js
@@ -0,0 +1,22 @@
+alert('welcome to my websit') 
+
+
+
+
+document.write("<a href=\"www.youtube.com\">");
+document.write("YouTube");
+document.write("</a>");
+
+var userPlatform = prompt('favourite learning platform')
+
+if(userChoice === 'google'){
+  alert('Google'.link('http://www.google.com'));
+
+}else if(userChoice === 'YouTube'){
+  document.write("<a href=\"https://www.youtube.com/\">")
+}else {
+  alert('you are bothway right')
+}
+
+
+
