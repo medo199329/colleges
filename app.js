36c1c8c00f6d91f9f392a850e43505311d50be00
MohammedSalah8 <>
2021-05-09 11:38:29 +0000

    new js

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-alert('welcome to my websit') 
+ var college1= 'harvard';
+ var college2= 'cambridge';
+
+ function add (college1, college2) {
+   return college1 + college2
+ }
+  
+ console.log(college1 + college2)
+ 
+      
+      
 
 
 
+      alert('welcome')
 
 document.write("<a href=\"www.youtube.com\">");
 document.write("YouTube");
@@ -9,14 +20,8 @@ document.write("</a>");
 
 var userPlatform = prompt('favourite learning platform')
 
-if(userChoice === 'google'){
+if ( userChoice === 'google'){
   alert('Google'.link('http://www.google.com'));
 
 }else if(userChoice === 'YouTube'){
-  document.write("<a href=\"https://www.youtube.com/\">")
-}else {
-  alert('you are bothway right')
-}
-
-
-
+  document.write("<a href=\"https://www.youtube.com/\">")}
